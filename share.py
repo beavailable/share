23764c8a7bbbb2bcc4af75a471ecc2abfe241d01
beavailable <beavailable@proton.me>
2024-01-12 18:24:27 +0800

    Remove an unused class name

--- share.py
+++ share.py
@@ -509,7 +509,7 @@ window.onload = on_load;
             builder.append('</li>')
         for f, hidden, size in files:
             builder.append('<li class="list-item">')
-            builder.append(f'<a class="btn-view item-left{" hidden" if hidden else ""}" href="{html.escape(parse.quote(f))}">')
+            builder.append(f'<a class="item-left{" hidden" if hidden else ""}" href="{html.escape(parse.quote(f))}">')
             builder.append('<svg class="item-icon" xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="#76797b"><path d="M320-240h320v-80H320v80Zm0-160h320v-80H320v80ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"/></svg>')
             builder.append(f'{html.escape(f)}')
             builder.append('</a>')
